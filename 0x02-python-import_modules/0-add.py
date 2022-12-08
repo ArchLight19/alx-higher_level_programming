86d7ce3ee28ffb8af0e61b9f905322ef9c30f914
ArchLight <65264591+ArchLight19@users.noreply.github.com>
2022-12-08 01:04:14 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    import add_0 as adding
+    """Print some of 1 and 2"""
+    from add_0 import add
+
     a = 1
     b = 2
-    c = adding.add(a, b)
-    print("{} + {} = {}".format(a, b, c))
+    print("{} + {} = {}".format(a, b, add(a, b)))
