92e95ac90a939e653099629a57625e570aaa306d
ArchLight <65264591+ArchLight19@users.noreply.github.com>
2022-12-22 02:51:08 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-''' A Module that creates a Square object '''
+"""This is a module that defines a class of a Square"""
 
-class Square;
-''' Creating an Object template '''
-    def __init__(self, size = 0):
-        '''
-            The init method initializes the class instance
 
-        @self:
-            A parameter used to refer to the class instance
+class Square:
+    """This is the Square class"""
 
-        @size:
-            The size of the square, must be a +ve integer
-        '''
-        if type(size) is int:
-            if size < 0:
-                raise ValueError('Size must be >= 0')
-            else:
-                self.__size = size
+    def __init__(self, size=0):
+        self.__setsize = size
+
+    @property
+    def __setsize(self):
+        return self.__size
+
+    @__setsize.setter
+    def __setsize(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
-            raise TypeError('Size must be an integer')
+            self.__size = size
