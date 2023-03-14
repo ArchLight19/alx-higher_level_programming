fc8017595bafd4d34f1cfd802248a56f83b62eab
ArchLight <65264591+ArchLight19@users.noreply.github.com>
2023-03-14 04:02:15 +0100

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar);
+const ae = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(ae);
